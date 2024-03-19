21873c5e0b3f009e84981bef77f4da4c8df7aec5
miguelVillaq <125335842+miguelVillaq@users.noreply.github.com>
2024-03-19 12:27:38 -0500

    Segmentación funcionando en interfaz sin anotaciones.

---
+++ main.py
@@ -0,0 +1,200 @@
+import segmentacion_functions as sf
+import tkinter as tk
+from tkinter import filedialog
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+class ImageProcessingApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Image Processing App")
+
+        self.image_data = None
+        self.segmentation_data = None
+
+        self.create_menu()
+        self.create_image_display()
+        self.create_algorithm_form()
+        self.create_navigation_bar()
+        
+    def create_menu(self):
+        menu_bar = tk.Menu(self)
+
+        # Menú Archivo
+        file_menu = tk.Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Abrir", command=self.open_image)
+        file_menu.add_separator()
+        file_menu.add_command(label="Reset", command=self.reset_image)
+        file_menu.add_command(label="Salir", command=self.quit)
+        menu_bar.add_cascade(label="Archivo", menu=file_menu)
+
+        # Menú segmentación.
+        algorithm_menu = tk.Menu(menu_bar, tearoff=0)
+        algorithm_menu.add_command(label="Umbralización", command=self.show_umbralization_form)
+        algorithm_menu.add_command(label="Isodata", command=self.show_isodata_form)
+        algorithm_menu.add_command(label="Region growing", command=self.show_rg_form)
+        algorithm_menu.add_command(label="K-Means", command=self.show_kmeans_form)
+        menu_bar.add_cascade(label="Segmentacion", menu=algorithm_menu)
+        
+        self.config(menu=menu_bar)
+        
+    def create_image_display(self):
+        self.right_frame = tk.Frame(self)
+        self.right_frame.pack(pady=10)
+
+        self.figure = plt.Figure(figsize=(5, 5), dpi=100)
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.right_frame)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        
+    def create_algorithm_form(self):
+        self.algorithm_frame = tk.Frame(self)
+        self.algorithm_frame.pack(pady=12)
+
+        self.algorithm_label = tk.Label(self.algorithm_frame, text="Parámetros de Algoritmo:")
+        self.algorithm_label.grid(row=0, column=0, columnspan=2, pady=5)
+
+        self.algorithm_entry_label_1 = tk.Label(self.algorithm_frame, text="Parámetro:")
+        self.algorithm_entry_label_1.grid(row=1, column=0, padx=5)
+
+        self.algorithm_entry_1 = tk.Entry(self.algorithm_frame)
+        self.algorithm_entry_1.grid(row=1, column=1, padx=5)
+        
+        self.algorithm_entry_label_2 = tk.Label(self.algorithm_frame, text="Parámetro:")
+        self.algorithm_entry_label_2.grid(row=2, column=0, padx=5)
+
+        self.algorithm_entry_2 = tk.Entry(self.algorithm_frame)
+        self.algorithm_entry_2.grid(row=2, column=1, padx=5)
+    
+        self.algorithm_entry_label_3 = tk.Label(self.algorithm_frame, text="Parámetro:")
+        self.algorithm_entry_label_3.grid(row=3, column=0, padx=5)
+
+        self.algorithm_entry_3 = tk.Entry(self.algorithm_frame)
+        self.algorithm_entry_3.grid(row=3, column=1, padx=5)
+        
+        self.algorithm_entry_label_4 = tk.Label(self.algorithm_frame, text="Parámetro:")
+        self.algorithm_entry_label_4.grid(row=4, column=0, padx=5)
+
+        self.algorithm_entry_4 = tk.Entry(self.algorithm_frame)
+        self.algorithm_entry_4.grid(row=4, column=1, padx=5)
+        
+        self.run_algorithm_button = tk.Button(self.algorithm_frame, text="Ejecutar", command=self.run_algorithm)
+        self.run_algorithm_button.grid(row=5, column=0, columnspan=2, pady=5)
+        
+    def create_navigation_bar(self):
+        self.navigation_frame = tk.Frame(self)
+        self.navigation_frame.pack(pady=10)
+
+        self.navigation_label = tk.Label(self.navigation_frame, text="Seleccionar eje:")
+        self.navigation_label.grid(row=0, column=0, padx=5)
+
+        self.navigation_var = tk.StringVar(self)
+        self.navigation_var.set("Z")
+        self.navigation_menu = tk.OptionMenu(self.navigation_frame, self.navigation_var, "X", "Y", "Z")
+        self.navigation_menu.grid(row=0, column=1, padx=5)
+
+        
+        self.navigation_scale = tk.Scale(self.navigation_frame, from_=0, to=100, orient="horizontal", command=self.update_image_display)
+        self.navigation_scale.grid(row=0, column=2, padx=5)
+
+    def open_image(self):
+        file_path = filedialog.askopenfilename(filetypes=[("NIfTI files", "*.nii"), ("All files", "*.*")])
+        if file_path:
+            image = nib.load(file_path)
+            self.image_data = image.get_fdata()
+            self.navigation_scale.config(to=self.image_data.shape[2]-1)
+            self.update_image_display()
+            
+    def update_image_display(self, event=None):
+        if self.image_data is not None:
+            current_slice = int(self.navigation_scale.get())
+            axis = self.navigation_var.get()
+
+            if axis == "X":
+                image_slice = self.image_data[current_slice, :, :]
+            elif axis == "Y":
+                image_slice = self.image_data[:, current_slice, :]
+            else:  # axis == "Z"
+                image_slice = self.image_data[:, :, current_slice]
+
+            self.figure.clear()
+            ax = self.figure.add_subplot(111)
+            ax.imshow(image_slice)
+            self.canvas.draw()
+        
+    def show_umbralization_form(self):
+        self.algorithm_label.config(text="Parámetros de Umbralización:")
+        self.algorithm_entry_label_1.config(text="Umbral:")
+        self.algorithm_entry_1.config(state='normal')
+        self.algorithm_entry_2.config(state='disabled')
+        self.algorithm_entry_3.config(state='disabled')
+        self.algorithm_entry_4.config(state='disabled')
+
+    def show_isodata_form(self):
+        self.algorithm_label.config(text="Parámetros de Isodata:")
+        self.algorithm_entry_label_1.config(text="Umbral:")
+        self.algorithm_entry_label_2.config(text="Tolerancia:")
+        self.algorithm_entry_1.config(state='normal')
+        self.algorithm_entry_2.config(state='normal')
+        self.algorithm_entry_3.config(state='disabled')
+        self.algorithm_entry_4.config(state='disabled')
+
+    
+    def show_kmeans_form(self):
+        self.algorithm_label.config(text="Parámetros de K-Means:")
+        self.algorithm_entry_label_1.config(text="K:")
+        self.algorithm_entry_label_2.config(text="Númnero iteraciones:")
+        self.algorithm_entry_1.config(state='normal')
+        self.algorithm_entry_2.config(state='normal')
+        self.algorithm_entry_3.config(state='disabled')
+        self.algorithm_entry_4.config(state='disabled')
+
+
+    def show_rg_form(self):
+        self.algorithm_label.config(text="Parámetros de Region growing:")
+        self.algorithm_entry_label_1.config(text="Tolerancia:")                   
+        self.algorithm_entry_label_2.config(text="X:") 
+        self.algorithm_entry_label_3.config(text="Y:")   
+        self.algorithm_entry_label_4.config(text="Z:")
+        self.algorithm_entry_1.config(state='normal')
+        self.algorithm_entry_2.config(state='normal')
+        self.algorithm_entry_3.config(state='normal')
+        self.algorithm_entry_4.config(state='normal')
+
+          
+        
+    def run_algorithm(self):
+        if self.algorithm_label.cget("text") == "Parámetros de Umbralización:":
+            umbral = float(self.algorithm_entry_1.get())
+            if self.image_data is not None:
+                self.image_data = sf.threshold(self.image_data, umbral)
+        elif self.algorithm_label.cget("text") == "Parámetros de Isodata:":
+            umbral = float(self.algorithm_entry_1.get())
+            tolerancia = float(self.algorithm_entry_2.get())
+            if self.image_data is not None:
+                self.image_data = sf.isodata(self.image_data, umbral, tolerancia)
+        elif self.algorithm_label.cget("text") == "Parámetros de Region growing:":
+            tolerancia = int(self.algorithm_entry_1.get())
+            x = int(self.algorithm_entry_2.get())
+            y = int(self.algorithm_entry_3.get())
+            z = int(self.algorithm_entry_4.get())
+            if self.image_data is not None:
+                self.image_data = sf.region_growing3D(self.image_data,tolerancia,x,y,z).astype(np.uint8)
+        elif self.algorithm_label.cget("text") == "Parámetros de K-Means:":
+            k = int(self.algorithm_entry_1.get())
+            num_iter = int(self.algorithm_entry_2.get())
+            if self.image_data is not None:
+                self.image_data = sf.kmeans(self.image_data, k, num_iter).astype(np.uint8)
+
+        self.update_image_display()
+        
+    def reset_image(self):
+            self.image_data = None
+            self.figure.clear()
+            self.canvas.draw() 
+            self.open_image()
+            
+app = ImageProcessingApp()
+app.mainloop()
---
+++ segmentacion_functions.py
@@ -0,0 +1,86 @@
+import numpy as np
+import nibabel as nib
+# import matplotlib.pyplot as plt
+
+def threshold(img, umbral):
+    img_umbral = img > umbral 
+    return img_umbral
+
+def isodata(img, umbral, tolerancia):
+    while True:
+        img_umbral = threshold(img, umbral)
+        img_foreground = img[img_umbral == 1].mean()
+        img_background = img[img_umbral == 0].mean()
+        umbral_new = (img_foreground + img_background)/2
+        if abs(umbral_new - umbral) < tolerancia:
+            return img_umbral
+        else:
+            umbral = umbral_new
+            
+def region_growing2D(img, tol, row, col, dep, img_bin, cluster, visited):
+  alto, ancho = img.shape[:2]
+  stack = [tuple([row, col, dep])]
+  while stack:
+    valor_comparacion = cluster.mean()
+    # Recorrido 8 vecinos.
+    for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1,1), (1,1), (1,-1), (-1,-1)]:
+      next_row, next_col = row + x, col + y
+      # Validaciones de que esté dentro de los límites imágenes y que no se haya revisado con anterioridad.
+      if 0 <= next_row < alto and 0 <= next_col < ancho:
+        if not (tuple([next_row, next_col, dep]) in visited):
+          # Comparación del valor de referencia, mete al stack, une al cluster, etc.
+          if abs( np.linalg.norm(valor_comparacion - img[next_row, next_col, dep])) <= tol:
+            cluster = np.append(cluster, [img[next_row, next_col, dep]], axis = 0)
+            img_bin[next_row, next_col, dep] = 1
+            stack.append([next_row, next_col, dep])
+            visited.add(tuple([next_row, next_col, dep]))
+          else:
+            #stack.append([next_row, next_col, dep])
+            visited.add(tuple([next_row, next_col, dep]))
+    row, col, dep = stack.pop(0)
+  return img_bin, cluster, visited
+
+def region_growing3D(img, tol, row, col, dep):
+  # Parámetros iniciales.
+  profundidad = img.shape[2]
+  valor_comparacion = img[row,col,dep]
+  img_bin = np.zeros_like(img, dtype=np.int8)
+  img_bin[row,col,dep] = 1
+  cluster = np.array([valor_comparacion])
+  visited = set()
+  stack_dep = []
+
+  # Ejecución inicial del algoritmo 2D, es decir, una sola profundidad.
+  img_ini, cluster, visited = region_growing2D(img, tol, row, col, dep, img_bin, cluster, visited)
+
+  # Ciclo que maneja cuántas iteraciones de profundidad se harán.
+  for i in range(5):
+    for z in [(1),(-1)]:
+      next_dep = dep + z
+      if 0 <= next_dep < profundidad:
+        if not (tuple([row, col, next_dep]) in visited):
+          img_ini, cluster, visited = region_growing2D(img, tol, row, col, next_dep, img_ini, cluster, visited)
+          stack_dep.append(next_dep)
+    dep = stack_dep.pop(0)
+  return img_ini
+
+def kmeans(img, k, num_iter):
+    # Crear los centroides iniciales aleatorios
+    cluster = np.random.choice(np.ravel(img[img > 0]), size=k, replace=False)
+    #print(cluster)
+    #cluster = np.linspace(np.amin(img), np.amax(img), k)
+
+    for _ in range(num_iter):  # Realizamos 10 iteraciones (puedes ajustar este valor según sea necesario)
+        # Calcular las distancias entre cada píxel y los centroides, con expand_dims, reducimos trabajo al calcular la resta sólo en la dimensión de profundidad.
+        distance = np.abs(np.expand_dims(img, axis=-1) - cluster)
+
+        # Asignar cada píxel al cluster más cercano: Segmentation contiene los índices del cluster al que pertenece la distancia más corta.
+        segmentation = np.argmin(distance, axis=-1)
+
+        # Actualizar los centroides como el promedio de los píxeles asignados a cada cluster
+        for k_cluster in range(k):
+            cluster[k_cluster] = np.mean(img[segmentation == k_cluster])
+
+    # Calcular la segmentación final
+    segmentation = np.argmin(np.abs(np.expand_dims(img, axis=-1) - cluster), axis=-1)
+    return segmentation
